c9752f8d4e900536cab449a43a25262441e5fcf3
Maciej B <maciek.b2k@gmail.com>
2019-10-12 20:51:46 +0200

    Upgraded webpack config for production

--- webpack.config.js
+++ webpack.config.js
@@ -1,83 +1,102 @@
 /* eslint-disable */
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-const config = {
-    entryFile: "./src/index.ts",
-    outputFile: "bundle.js",
-    outputFolder: "dist"
-}
+const entryFile = "./src/index.ts";
+const outputFile = "_bundle.js";
+const outputFolder = "dist";
 
-module.exports = {
-    mode: "development",
-    entry: config.entryFile,
-    output: {
-        path: path.resolve(__dirname, config.outputFolder),
-        filename: config.outputFile,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                loader: "ts-loader",
-                exclude: /node_modules/,
+module.exports = (env, argv) => ({
+  mode: argv.mode,
+  entry: entryFile,
+  output: {
+    path: path.resolve(__dirname, outputFolder),
+    filename: outputFile,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loader: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.js$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          { loader: "style-loader" },
+          { loader: MiniCssExtractPlugin.loader },
+          { loader: "css-loader" },
+          {
+            loader: "sass-loader",
+            options: {
+              implementation: require("sass"),
             },
-            {
-                test: /\.js$/,
-                loader: "babel-loader",
-                exclude: /node_modules/,
-            },
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    {loader: MiniCssExtractPlugin.loader},
-                    {loader: "style-loader"},
-                    {loader: "css-loader"},
-                    {
-                        loader: "sass-loader",
-                        options: {
-                            implementation: require("sass"),
-                        },
-                    },
-                ],
-            },
-            {
-                test: /\.(png|jpe?g|gif|svg)$/,
-                use: [
-                    {
-                        loader: "file-loader",
-                        options: {
-                            outputPath: "images/",
-                        },
-                    },
-                ],
-            },
-            {
-                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-                use: [
-                    {
-                        loader: "file-loader",
-                        options: {
-                            outputPath: "fonts/",
-                        },
-                    },
-                ],
-            },
+          },
         ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./src/template/App.html",
-            filename: "index.html",
-        }),
-        new MiniCssExtractPlugin({
-            filename: '[name].css',
-        }),
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          { loader: "style-loader" },
+          { loader: MiniCssExtractPlugin.loader },
+          { loader: "css-loader" },
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              outputPath: "images/",
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              outputPath: "fonts/",
+            },
+          },
+        ],
+      },
     ],
-    resolve: {
-        alias: {
-            app: path.join(__dirname, "..", "src")
-          }
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new CaseSensitivePathsPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./src/template/app.html",
+      filename: "index.html",
+    }),
+    new CopyWebpackPlugin([{
+      from: 'src/images',
+      to: 'images'
+    }]),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+      ignoreOrder: false,
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve("src"),
     },
-};
+    extensions: [".js", ".json", ".ts", ".scss", ".css"],
+  },
+  devtool: argv.mode === "production" ? "source-map" : "inline-source-map",
+});
